45e98d8bf05f4aa7aba06a62e0909d0cd9d7c9d3
Bruno Moacir <brunopmoacir129@gmail.com>
2025-05-06 15:08:03 -0300

    Exercicio 3

---
+++ Aula21/exercicio3.c
@@ -0,0 +1,8 @@
+void copiaMatriz(float M[MAX_LIN][MAX_COL], float N[MAX_LIN][MAX_COL])
+{
+    for(int i = 0; i < MAX_LIN; i++){
+        for (int j = 0; j < MAX_COL; j++){
+            N[i][j]= M[i][j]
+        }
+    }
+}
