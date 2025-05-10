9977a7206cfb5af5f82ed73d1b2d45e35d039fbc
Bruno Moacir <brunopmoacir129@gmail.com>
2025-05-10 11:39:16 -0300

    ex 1

---
+++ exerciciosVetoresC-treino/1.c
@@ -0,0 +1,14 @@
+// ler vetor e mostrar o vetor
+
+int main ()
+{
+    int i;
+    float vet [4];
+    for (int i = 0; i < 4; i++){
+        scanf("%f", vet[i]);
+    }
+
+    for(int i = 0; i < 4; i++){
+        printf ("%f\n", vet[i]);
+    }
+}
