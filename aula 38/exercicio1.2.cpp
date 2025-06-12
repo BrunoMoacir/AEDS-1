6f288a5d295ee268ce0df6d5f6c4cfb06312065c
Bruno Moacir <brunopmoacir129@gmail.com>
2025-06-11 21:26:54 -0300

    treinei mais c++

--- aula 38/exercicio1.2.cpp
+++ aula 38/exercicio1.2.cpp
@@ -1,64 +1,69 @@
-#include <iostream>
-#include <string>
-#define _MAX 100
-
-using namespace std;
-
+#define MAX 100
 class Candidato{
-    private:
+private:
     string nome;
     double nota;
-
-    public:
-    //CONSTRUTOR
-    // o que garante que sempre que um candidato for criado ele nascer com um nome e uma nota
-    Candidato(string nomeInicial, double notaInicial){
-        nome = nomeInicial;
-        idade = idadeInicial;
+public:
+    Candidato(string nome, double nota){ //CONSTRUTOR
+        this->nome = nome;
+        this->nota = nota
     }
-
+    // metodos para acessar e modificar os dados de forma segura
     string getNome(){
         return nome;
     }
     double getNota(){
         return nota;
     }
-    //metodos set: usados para definir/alterar o valor de atributo privado
-    void setNome(string novoNome){
-        nome = novoNome;
+    void setNome(string nome){
+        this->nome = nome;
     }
-    void setNota(double novaNota){
-        nota = novaNota;
-    }
-    //get e set sao para ler dados que estao privados de forma segura
-    //funcao para ler candidatos
-    void lerCandidato(Candidato* candidatos[],int n ){
-        string nome;
-        double nota;
-        for(int i = 0; i < n; i ++){
-            cout<<"Digite o nome do candidato: ";
-            cin>>nome;
-            cout<<"Digite a nota do candidato: ";
-            cin>>nota;
-            candidatos[i] = new Candidato(nome,nota);
-        }
-    }
-    // calculo da media
-    double calculoMedia(Candidato* candidatos[],int n){
-        double soma = 0;
-        for(int i = 0; i < n; i++){
-            soma += candidatos[i]->getNota();
-        }
-        return soma/n;
-    }
-    //listar os candidatos acima media
-    void listaCandidatosAcima(Candidato* candidatos[],int n, double media){
-        cout<<"Candidatos acima da media";
-        for(int i = 0; i < n; i++){
-            if(candidatos[i]->getNota() > media){
-                cout<<candidatos[i]->getNome()<<"-"<<candidatos[i]->getNota()<<endl;
-            }
-        }
+    void setNota(double nota){
+        this->nota = nota;
     }
 };
-int main
+int quantidadeCandidatos(){
+    int n;
+    do{
+        cout<<"Digite o numero de candidatos:";
+        cin>>n;
+    }while(n<1 || n > MAX);
+    return n;
+}
+void criarCandidatos(int n,Candidato* candidatos[]){// construtora
+    for(int i = 0; i < n; i++){
+        string nome;
+        double nota;
+        cout<<"nome: ";
+        getline(cin,nome);
+        cout<<"nota:";
+        cin>>nota;
+        // aloca memoria para um novo Candidato e guarda seu endereço no vetor 
+        candidatos [i] = new Candidato(nome,nota); 
+    }
+}// a funcao cria um novo candidato na memoria com o new e armazena o ponteiro para ele no seu vetor
+double media(int n, Candidato* candidatos[]){
+    double soma = 0;
+    for(int i = 0; i < n; i++){
+        soma += candidatos[i]->getNota();// usa -> para acessar via PONTEIRO
+    }
+    return soma / n;
+}
+void listaAcimaMedia(int n, Candidato* candidato[], double media){
+    cout<<"Candidatos acima da media: ";
+    for(int i = 0; i < n; i ++){
+        if(candidatos[i]->getNota() > media){
+            cout<<candidatos[i]->getNome()<<"-"<<candidatos[i]->getNota();
+    }
+}
+int main(){
+    Candidato* candidatos[MAX];
+    int n = qtCandidatos();
+    criarCandidatos(n,candidatos);
+    double media = notaMedia(n , candidatos);
+    listasAcimadaMedia(n,candidatos,media);
+    for(int i = 0; i<n;i++){
+        delete candidatos[i];
+    }
+    return 0;
+}
